0c5440a510f5257921d6fe5ba81adb026499523a
CodeHart-art <jobhart11211@gmail.com>
2026-04-27 11:13:06 -0400

    Method for reading csv and creating arraylist

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,45 @@
-package PACKAGE_NAME;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
 
 public class Main {
+    public static void main(String[] args) {
+       ArrayList<Transaction> transactions = transactionList();
+
+
+
+    }
+
+    private static ArrayList<Transaction> transactionList() {
+        ArrayList<Transaction> transactions = new ArrayList<>();
+
+        try {
+            FileReader fileReader = new FileReader("src/main/resources/transactions.csv");
+            BufferedReader buffReader = new BufferedReader(fileReader);
+
+
+            String line;
+            buffReader.readLine();
+            while ((line = buffReader.readLine()) != null) {
+
+                String[] transactionInfo = line.split("\\|");
+
+                Transaction transaction = new Transaction(
+                        LocalDate.parse(transactionInfo[0]),
+                        LocalTime.parse(transactionInfo[1]),
+                        transactionInfo[2],
+                        transactionInfo[3],
+                        Double.parseDouble(transactionInfo[4]));
+                transactions.add(transaction);
+            }
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return transactions;
+    }
 }
--- src/main/java/Transaction.java
+++ src/main/java/Transaction.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
-public class Traansaction {
+public class Transaction {
+    LocalDate transactionDate;
+    LocalTime transactionTime;
+    String description;
+    String vendor;
+    double transactionAmount;
+
+    public Transaction(LocalDate transactionDate, LocalTime transactionTime, String description, String vendor, double transactionAmount) {
+        this.transactionDate = transactionDate;
+        this.transactionTime = transactionTime;
+        this.description = description;
+        this.vendor = vendor;
+        this.transactionAmount = transactionAmount;
+    }
+
+    @Override
+    public String toString(){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        String outPutTime = transactionTime.format(formatter);
+        return transactionDate + "|" + outPutTime + "|" + description + "|" + vendor + "|" + transactionAmount;
+    }
 }
